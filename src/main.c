b72e8b162f1285b2110c80990666d66cb2774d88
Valentin Zickner <zickner@querformatik.de>
2014-12-25 23:43:20 +0100

    Splitted between server and sender dummy.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,13 @@
+//#define NODE_SERVER
+#define NODE_DUMMY
+
+#ifdef NODE_SERVER
 #include "../lib/avr-uart/uart.h"
 void uart_putstr(char *str) {
 	uart_puts(str);
 }
+#endif
+
 #include "config/rfm12_config.h"
 #include "../lib/librfm12/src/rfm12.h"
 #include "../lib/librfm12/src/rfm12.c"
@@ -12,23 +18,26 @@ void uart_putstr(char *str) {
 int main() {
 	uint8_t *bufptr;
 	uint16_t ticker = 0;
-	uint8_t tv[] = "foobar";
+	uint8_t tv[] = "yeah";
 	uint8_t packettype = 0xEE;
+#ifdef NODE_SERVER
 	uart0_init(UART_BAUD_SELECT(BAUD, F_CPU));
+#endif
+
 	_delay_ms(100);
 	rfm12_init();
 	sei();
 	uint8_t i = 0;
 
-	uart_puts ("\r\n" "RFM12 Pingpong test\r\n");
-
+#ifdef NODE_SERVER
+	uart_puts ("\n" "RFM12 Server\n");
+#endif
 	
 	while (1==1) {
+#ifdef NODE_SERVER
                 if (rfm12_rx_status() == STATUS_COMPLETE)
                 {
                         //so we have received a message
-
-                        uart_puts ("new packet: \"");
 
                         bufptr = rfm12_rx_buffer(); //get the address of the current rx buffer
 
@@ -38,20 +47,21 @@ int main() {
                                 uart_putc ( bufptr[i] );
                         }
 
-                        uart_puts ("\"\r\n");
+                        uart_puts ("\n");
 
                         // tell the implementation that the buffer
                         // can be reused for the next data.
                         rfm12_rx_clear();
                 }
+#endif
 
-
+#ifdef NODE_DUMMY
                 ticker ++;
                 if(ticker == 3000){
                         ticker = 0;
-                        uart_puts (".");
                         rfm12_tx (sizeof(tv), 0, tv);
                 }
+#endif
 
                 //rfm12 needs to be called from your main loop periodically.
                 //it checks if the rf channel is free (no one else transmitting), and then
